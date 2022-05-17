62c43408e77f4c42209ba81dbf539adbc949d140
Suresh Kumar <sureshkumar23102000@gmail.com>
2022-05-17 23:41:14 +0530

    Hash user password using bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -17,6 +17,14 @@ var schema = new mongoose.Schema({
   },
 });
 
+// Hash password using bcrypt
+schema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  let newPassword = this.password.toString();
+  this.password = await bcrypt.hash(newPassword, salt);
+  next();
+});
+
 const User = mongoose.model("user", schema);
 
 module.exports = User;
